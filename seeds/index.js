e0c936c079ea4f01918709299965284be2acc91f
Nadeem_khan <nk8452868@gmail.com>
2025-01-23 12:14:49 +0530

    adding encription

--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,7 @@ const Gmail_temp = require("../models/index");
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://IzAIzIsFtL2svnVv:IzAIzIsFtL2svnVv@cluster0.uponu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  await mongoose.connect(process.env.DB_URL)
 console.log("connected to database")
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
